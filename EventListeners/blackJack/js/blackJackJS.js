21f6926bfd566b2474f9c93fe7dbdf94f7443bf8
John Kearney <John.d.kearney1@gmail.com>
2020-02-09 21:58:13 -0600

    done with setting up deck

---
+++ EventListeners/blackJack/js/blackJackJS.js
@@ -0,0 +1,87 @@
+// CREATE OUR DECK OF CARDS
+let deckOfCards = [];
+//--------------------- 2 -------------------------------------------
+let card1 = { suit: 'Clubs', value: 2, imageURL: 'images/2C.jpg' };
+let card2 = { suit: 'Diamonds', value: 2, imageURL: 'images/2D.jpg' };
+let card3 = { suit: 'Hearts', value: 2, imageURL: 'images/2H.jpg' };
+let card4 = { suit: 'Spades', value: 2, imageURL: 'images/2S.jpg' };
+
+//--------------------- 3 -------------------------------------------
+let card5 = { suit: 'Clubs', value: 3, imageURL: 'images/3C.jpg' };
+let card6 = { suit: 'Diamonds', value: 3, imageURL: 'images/3D.jpg' };
+let card7 = { suit: 'Hearts', value: 3, imageURL: 'images/3H.jpg' };
+let card8 = { suit: 'Spades', value: 3, imageURL: 'images/3S.jpg' };
+
+//--------------------- 4 -------------------------------------------
+let card9 = { suit: 'Clubs', value: 4, imageURL: 'images/4C.jpg' };
+let card10 = { suit: 'Diamonds', value: 4, imageURL: 'images/4D.jpg' };
+let card11 = { suit: 'Hearts', value: 4, imageURL: 'images/4H.jpg' };
+let card12 = { suit: 'Spades', value: 4, imageURL: 'images/4S.jpg' };
+
+//--------------------- 5 -------------------------------------------
+let card13 = { suit: 'Clubs', value: 5, imageURL: 'images/5C.jpg' };
+let card14 = { suit: 'Diamonds', value: 5, imageURL: 'images/5D.jpg' };
+let card15 = { suit: 'Hearts', value: 5, imageURL: 'images/5H.jpg' };
+let card16 = { suit: 'Spades', value: 5, imageURL: 'images/5S.jpg' };
+
+//--------------------- 6 -------------------------------------------
+let card17 = { suit: 'Clubs', value: 6, imageURL: 'images/6C.jpg' };
+let card18 = { suit: 'Diamonds', value: 6, imageURL: 'images/6D.jpg' };
+let card19 = { suit: 'Hearts', value: 6, imageURL: 'images/6H.jpg' };
+let card20 = { suit: 'Spades', value: 6, imageURL: 'images/6S.jpg' };
+
+//--------------------- 7 -------------------------------------------
+let card21 = { suit: 'Clubs', value: 7, imageURL: 'images/7C.jpg' };
+let card22 = { suit: 'Diamonds', value: 7, imageURL: 'images/7D.jpg' };
+let card23 = { suit: 'Hearts', value: 7, imageURL: 'images/7H.jpg' };
+let card24 = { suit: 'Spades', value: 7, imageURL: 'images/7S.jpg' };
+
+//--------------------- 8 -------------------------------------------
+let card25 = { suit: 'Clubs', value: 8, imageURL: 'images/8C.jpg' };
+let card26 = { suit: 'Diamonds', value: 8, imageURL: 'images/8D.jpg' };
+let card27 = { suit: 'Hearts', value: 8, imageURL: 'images/8H.jpg' };
+let card28 = { suit: 'Spades', value: 8, imageURL: 'images/8S.jpg' };
+
+//--------------------- 9 -------------------------------------------
+let card29 = { suit: 'Clubs', value: 9, imageURL: 'images/9C.jpg' };
+let card30 = { suit: 'Diamonds', value: 9, imageURL: 'images/9D.jpg' };
+let card31 = { suit: 'Hearts', value: 9, imageURL: 'images/9H.jpg' };
+let card32 = { suit: 'Spades', value: 9, imageURL: 'images/9S.jpg' };
+
+//--------------------- 10 -------------------------------------------
+let card33 = { suit: 'Clubs', value: 10, imageURL: 'images/10C.jpg' };
+let card34 = { suit: 'Diamonds', value: 10, imageURL: 'images/10D.jpg' };
+let card35 = { suit: 'Hearts', value: 10, imageURL: 'images/10H.jpg' };
+let card36 = { suit: 'Spades', value: 10, imageURL: 'images/10S.jpg' };
+
+//--------------------- Jack -------------------------------------------
+let card37 = { suit: 'Clubs', value: 10, imageURL: 'images/JC.jpg' };
+let card38 = { suit: 'Diamonds', value: 10, imageURL: 'images/JD.jpg' };
+let card39 = { suit: 'Hearts', value: 10, imageURL: 'images/JH.jpg' };
+let card40 = { suit: 'Spades', value: 10, imageURL: 'images/JS.jpg' };
+
+//--------------------- Queen -------------------------------------------
+let card41 = { suit: 'Clubs', value: 10, imageURL: 'images/QC.jpg' };
+let card42 = { suit: 'Diamonds', value: 10, imageURL: 'images/QD.jpg' };
+let card43 = { suit: 'Hearts', value: 10, imageURL: 'images/QH.jpg' };
+let card44 = { suit: 'Spades', value: 10, imageURL: 'images/QS.jpg' };
+
+//--------------------- King -------------------------------------------
+let card45 = { suit: 'Clubs', value: 10, imageURL: 'images/KC.jpg' };
+let card46 = { suit: 'Diamonds', value: 10, imageURL: 'images/KD.jpg' };
+let card47 = { suit: 'Hearts', value: 10, imageURL: 'images/KH.jpg' };
+let card48 = { suit: 'Spades', value: 10, imageURL: 'images/KS.jpg' };
+
+//--------------------- Ace -------------------------------------------
+let card49 = { suit: 'Clubs', value: 10, imageURL: 'images/AC.jpg' };
+let card50 = { suit: 'Diamonds', value: 10, imageURL: 'images/AD.jpg' };
+let card51 = { suit: 'Hearts', value: 10, imageURL: 'images/AH.jpg' };
+let card52 = { suit: 'Spades', value: 10, imageURL: 'images/AS.jpg' };
+
+var varToPush = '';
+for (let i = 1; i <= 52; i++) {
+	// window[varToPush] = 'card' + i.toString();
+	varToPush = eval('card' + i.toString());
+	deckOfCards.push(varToPush);
+}
+console.log(deckOfCards);
