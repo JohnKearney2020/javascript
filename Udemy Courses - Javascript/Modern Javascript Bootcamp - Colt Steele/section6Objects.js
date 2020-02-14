5abec9e556c4e7c699a2052efc1951ddc3b8674d
John Kearney <John.d.kearney1@gmail.com>
2020-02-13 23:21:49 -0600

    started to do list

--- Udemy Courses - Javascript/Modern Javascript Bootcamp - Colt Steele/section6Objects.js
+++ Udemy Courses - Javascript/Modern Javascript Bootcamp - Colt Steele/section6Objects.js
@@ -1,9 +1,11 @@
-const fitBitData = {
-	totalSteps: 308727,
-	totalMiles: 211.7,
-	avgCalorieBurn: 5744,
-	workoutsThisWeek: '5 of 7',
-	avgGoodSleep: '2:13',
-	// 45: 'forty five' //all keys are converted to strings, this will throw an error
-	'45': 'forty five'
-};
+// const fitBitData = {
+// 	totalSteps: 308727,
+// 	totalMiles: 211.7,
+// 	avgCalorieBurn: 5744,
+// 	workoutsThisWeek: '5 of 7',
+// 	avgGoodSleep: '2:13',
+// 	// 45: 'forty five' //all keys are converted to strings, this will throw an error
+// 	'45': 'forty five' //this will not work either
+// };
+
+let data = { a: 1 };
